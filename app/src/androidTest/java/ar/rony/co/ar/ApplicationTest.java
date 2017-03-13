2d9e9f9e154a539204208001be7aa81c6e185471
Tomersteps <tomer@stepsand.com>
2017-03-13 16:10:02 +0200

    first commit

---
+++ app/src/androidTest/java/ar/rony/co/ar/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ar.rony.co.ar;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
